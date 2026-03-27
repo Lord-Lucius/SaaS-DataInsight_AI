5d848784ca974d7b06495e79c16adb8969a74e58
luluzuri <luluzuri@student.42.fr>
2026-03-27 23:17:55 +0100

    feat[requirements.txt]: Added some python dependencies

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+
+list_files: list = []
+
 app = FastAPI()
 
 @app.get('/')
 def root():
 	return {"hello": "world"}
 
-@app.get('/health')
+@app.get('/health/')
 def health_check():
 	return {"status": "healthy"}
