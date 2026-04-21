92ffbb9a482e929b32069f038d0b7c0792e7e6ad
luluzuri <luluzuri@student.42.fr>
2026-04-21 23:03:12 +0200

    feature[]: Some basic check on the input csv file
    checks list:
       - MIME Check
       - Extension
       - Size ( 5 mb for now )

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     APP_NAME: str = "SaaS-DataInsight-AI"
     APP_VERSION: str = "0.1.0"
     DATABASE_URL: str = "postgresql://user:password@db:5432/saas_db"
+    MAX_FILE_SIZE_MB: int = 5
 
     model_config = {"env_file": ".env", "extra": "ignore"}
 
---
+++ backend/validators/validators.py
@@ -0,0 +1,14 @@
+import pathlib
+from fastapi import UploadFile, HTTPException
+from backend.core.config import settings
+
+async def check_csv_validity(file: UploadFile):
+	if file.content_type != "text/csv":
+		raise HTTPException(status_code=415, detail="File is not a csv")
+	if pathlib.Path(file.filename).suffix != ".csv":
+		raise HTTPException(status_code=400, detail="file extension miss matched")
+	content = await file.read()
+	if len(content) >= (settings.MAX_FILE_SIZE_MB * 1024 * 1024):
+		raise HTTPException(status_code=413, detail="file size too big")
+	await file.seek(0)
+	return content
